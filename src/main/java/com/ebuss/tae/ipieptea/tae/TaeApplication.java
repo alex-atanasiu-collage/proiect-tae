45de2f150ba282484a2afbfeddab689fead71acf
Alexandru Atanasiu <alexandru.atanasiu@outlook.com>
2018-05-19 11:10:05 +0300

    initial commit

---
+++ src/main/java/com/ebuss/tae/ipieptea/tae/TaeApplication.java
@@ -0,0 +1,12 @@
+package com.ebuss.tae.ipieptea.tae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ebuss/tae/ipieptea/tae/TaeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ebuss.tae.ipieptea.tae;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
